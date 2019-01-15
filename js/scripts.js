ff4dddd6391a2809bff7af9ec4b1938703befd1b
Denny Le and Shawn Lunsford <denny@>
2019-01-14 16:36:15 -0800

    create script.jss with prompts and word play, and insert some html

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var sentence = prompt("Enter a sentence");
+var stringLength = sentence.length;
+var firstChar = sentence.charAt(0);
+var lastChar = sentence.charAt(stringLength - 1);
+alert((firstChar.toUpperCase()) + (lastChar.toUpperCase()));
+alert((lastChar.toUpperCase()) + (firstChar.toUpperCase()));
+var third = sentence + (lastChar.toUpperCase()) + (firstChar.toUpperCase()) + ".";
+alert(third);
+var halfLength = parseInt((stringLength) / 2);
+alert(halfLength);
+var fourth = sentence.charAt(halfLength-0) + third;
+alert(fourth);
+var splitString = fourth.split("");
+var reverseArray = splitString.reverse();
+var joinArray = reverseArray.join("");
+alert(joinArray);
